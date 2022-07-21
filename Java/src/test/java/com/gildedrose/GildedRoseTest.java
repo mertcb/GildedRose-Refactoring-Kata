6eb7dc034cb1991702ef9d1a9ac7db56a73b2ff2
Mert Bilgiç <mert.bilgic@trendyol.com>
2022-07-21 16:34:44 +0300

    %100 coverage

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,10 +10,21 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        CombinationApprovals.verifyAllCombinations(
+            this::test_item,
+            new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+            new Integer[]{-1, 5, 0, 1,13},
+            new Integer[]{0, 20, 49, 10, 75}
+        );
+    }
+
+    private String test_item(String name, int sellIn, int quality){
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        return app.items[0].toString();
     }
 
 }
